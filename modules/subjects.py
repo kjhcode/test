6aa106cdb77c2938aaa1e1cca9dd572cac832d8f
joung hee <84551246+kjhcode@users.noreply.github.com>
2025-06-20 14:33:38 +0900

    Delete modules directory

--- modules/subjects.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def display_subject_area(title, description_dict):
-    st.subheader(title)
-    cols = st.columns(len(description_dict))
-    for i, (area, desc) in enumerate(description_dict.items()):
-        with cols[i]:
-            st.markdown(f"### {area}", unsafe_allow_html=True)
-            st.write(desc)
